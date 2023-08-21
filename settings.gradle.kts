b59b06043ac8285611e204e86f2b56c7ae353254
judynj47 <judyswaceke@gmail.com>
2023-08-21 10:22:08 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "August Morning Intents and Lists"
+include(":app")
